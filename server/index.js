0ce5b590f7baa05b43719ebe73a84346a42b5979
gitkrak <dpemdnjem23@gamail.com>
2021-12-18 22:52:21 +0900

    s

--- server/index.js
+++ server/index.js
@@ -48,21 +48,10 @@ app.use("/image", imageRouter);
 
 //server
 const HTTPS_PORT = process.env.HTTPS_PORT || 80;
-let server;
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = {key: privateKey, cert: certificate};
 
-  server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () =>
-    console.log(` 🚀 Server is starting on https ${HTTPS_PORT}`),
-  );
-  //인증서 없는경우
-} else {
   app.listen(HTTPS_PORT, () => {
     console.log(`      🚀 Server is starting on http ${HTTPS_PORT}`);
   });
-}
+
 
 module.exports = app;
